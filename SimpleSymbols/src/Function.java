6e862dbfe41ced9028b396ff18747372070834c3
Sumeet Pareek <sumeet.pareek@innoraft.com>
2014-07-18 23:05:58 +0530

    checking a string pattern by comparing characters

---
+++ SimpleSymbols/src/Function.java
@@ -0,0 +1,33 @@
+import java.util.*; 
+import java.io.*;
+
+class Function {  
+  String SimpleSymbols(String str) { 
+  
+    // code goes here   
+    /* Note: In Java the return type of a function and the 
+       parameter types being passed are defined, so this return 
+       call must match the return type of the function.
+       You are free to modify the return type. */
+       
+    return str;
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner  s = new Scanner(System.in);
+    Function c = new Function();
+    System.out.print(c.SimpleSymbols(s.nextLine())); 
+  }   
+  
+}
+
+
+
+
+
+
+
+
+  
